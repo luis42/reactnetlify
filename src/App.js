45c76c59aaec3533f9088af147f060ec02c7ba0c
luis42 <vivi_luis15@hotmail.com>
2020-11-06 23:11:52 -0600

    * Change in app

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <h1>Hola Sandy próximamente tendré una página web </h1>
       </header>
     </div>
   );
